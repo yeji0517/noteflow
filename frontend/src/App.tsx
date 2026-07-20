3b54921a5d7046043fe6c8add79e57030aa3c072
박예지 <gksquf517@gmail.com>
2026-07-20 20:00:52 +0900

    chore: setup project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
+import Home from './pages/Home';
+
 function App(){
-  return (
-    <div>
-      <h1>NoteFlow</h1>
-      <p>나만의 노트 앱</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <main className="min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold p-10">
+                NoteFlow
+            </h1>
+        </main>
+    );
+}
+
+export default Home;
